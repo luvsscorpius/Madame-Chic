23963b2022e097c90750ffdeae367876e44b741d
and <andersonvittor1801@gmail.com>
2023-06-18 20:23:03 -0300

    Terceiro commit

--- js/scripts.js
+++ js/scripts.js
@@ -14,11 +14,10 @@ const btn_addRoupa = document.querySelector('#btn_addRoupa')
 let a_roupas = []
 
 class Roupa { // Classe PAI/BASE
-    constructor(nome, marca, quantidade, foto, tipo) {
+    constructor(nome, marca, quantidade, tipo) {
         this.nome = nome
         this.marca = marca
         this.quantidade = quantidade
-        this.foto = foto
         this.tipo = tipo
     }
 }
@@ -27,41 +26,35 @@ btn_addRoupa.addEventListener('click', () => {
     console.log(f_select.value)
     if (f_select.value == "camisa") {
         console.log("Selecionei camisa")
-        const r = new Roupa(f_nome.value, f_marca.value, f_quantidade.value, f_imagem.src, f_select.value)
+        const r = new Roupa(f_nome.value, f_marca.value, f_quantidade.value, f_select.value)
         a_roupas.push(r)
     }
 
     if (f_select.value == "calca") {
         console.log("Selecionei Calça")
-        const r = new Roupa(f_nome.value, f_marca.value, f_quantidade.value, f_imagem.src, f_select.value)
+        const r = new Roupa(f_nome.value, f_marca.value, f_quantidade.value, f_select.value)
         a_roupas.push(r)
     }
 
-    if (f_select.value == "cropped") {
-        console.log("Selecionei cropped")
-        const r = new Roupa(f_nome.value, f_marca.value, f_quantidade.value, f_imagem.src, f_select.value)
+    if (f_select.value == "sapato") {
+        console.log("Selecionei sapato")
+        const r = new Roupa(f_nome.value, f_marca.value, f_quantidade.value, f_select.value)
         a_roupas.push(r)
     }
-
     gerenciarExibicaoRoupas()
 })
 
-const h2 = document.createElement('h2')
-h2.innerHTML = "Camisa"
-roupas.appendChild(h2)
-
-const roupaItem = document.createElement('div')
-roupaItem.setAttribute("class", "roupaItem")
-roupas.appendChild(roupaItem)
-
 const gerenciarExibicaoRoupas = () => {
+    roupas.innerHTML = ""
     a_roupas.forEach((r) => {
-        const camisa = document.createElement('div')
-        camisa.setAttribute("class", "roupa")
-        camisa.innerHTML = `Nome: ${r.nome}</br>`
-        camisa.innerHTML += `Marca: ${r.marca}</br>`
-        camisa.innerHTML += `Quantidade: ${r.quantidade}</br>`
-        camisa.innerHTML += `Tipo: ${r.tipo}`
-        roupaItem.appendChild(camisa)
+        const div = document.createElement('div')
+        div.setAttribute("class", "roupa")
+        div.setAttribute("data-nome", r.nome)
+        div.innerHTML = `Nome: ${r.nome}<br/>`
+        div.innerHTML += `Marca: ${r.marca}<br/>`
+        div.innerHTML += `Quantidade: ${r.quantidade}<br/>`
+        div.innerHTML += `Tipo: ${r.tipo}<br/>`
+        roupas.appendChild(div)
     })
 }
+
